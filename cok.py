4b1274e7f3dd008c1d6c669a08c25ad2e7372e1c
AscendoHack <52598832+AscendoHack@users.noreply.github.com>
2019-07-07 19:04:28 +0700

    Add files via upload

---
+++ cok.py
@@ -0,0 +1,107 @@
+import requests
+import mechanize
+import os
+import random
+import time
+os.system('clear')
+
+
+print "\033[1;32m================================="
+print "= Author : Jancok Anjing        ="
+print "= Group  : AscendoHack          ="
+print "= User   : Python               ="
+print "================================="
+os.system('date')
+
+print "\033[1;35m-----------------------------"
+print "=>MENU<="
+print "-----------------------------"
+print "=>Dark Fb"
+print "-----------------------------"
+print "=>Spam sms"
+print "-----------------------------"
+print "=>Spam Gojek"
+print "-----------------------------"
+print "=>Spam Telkomsel"
+print "-----------------------------"
+print "=>Bengkel Facebook Chekpoint"
+print "-----------------------------"
+print "\033[1;33m"
+pilih = raw_input("[<>]MASUKAN PILIHAN : ")
+
+if pilih == 'Dark Fb':
+   print "\033[1;38mLOGIN LICENSE"
+   pilih = raw_input("USERNAME : ")
+   pilih = raw_input("PASSWORD : ")
+   print "PLEASE WAIT.."
+   time.sleep(10)
+   print "LOGIN GAGAL.."
+   os.system('xdg-open https://mytreep.icu/4vKP3')
+   os.system('python2 lol.py')
+elif pilih == 'Spam sms':
+   print ("PASTIKAN KOUTA ANDA LEBIH DARI 1GB KARENA INI MEMAKAN BANYAK DATA")
+   pilih = raw_input("MASUKAN NOMOR KORBAN :")
+   print ("Anda Habis ini akan masuk ke halaman. Jika anda tidak tekan Get Link maka script tidak akan work")
+   time.sleep(3)
+   os.system('xdg-open https://mytreep.icu/4vKP3')
+   time.sleep(9)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES DIKIRIM"
+elif pilih == 'Spam Gojek':
+   print ("PASTIKAN KOUTA ANDA LEBIH DARI 1GB KARENA INI MEMAKAN BANYAK DATA")
+   pilih = raw_input("MASUKAN NOMOR KORBAN :")
+   print ("Anda Habis ini akan masuk ke halaman. Jika anda tidak tekan Get Link maka script tidak akan work")
+   time.sleep(3)
+   os.system('xdg-open https://mytreep.icu/4vKP3')
+   time.sleep(9)
+   print "[!]SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES DIKIRIM"
+elif pilih == 'Spam Telkomsel':
+   print ("PASTIKAN KOUTA ANDA LEBIH DARI 1GB KARENA INI MEMAKAN BANYAK DATA")
+   pilih = raw_input("MASUKAN NOMOR KORBAN :")
+   print ("Anda Habis ini akan masuk ke halaman. Jika anda tidak tekan Get Link maka script tidak akan work")
+   time.sleep(3)
+   os.system('xdg-open https://mytreep.icu/4vKP3')
+   time.sleep(9)
+   print "[!]SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES"
+   time.sleep(3)
+   print "SUKSES DIKIRIM"
+elif pilih == 'Bengkel Facebook Chekpoint':
+   print "LOGIN LICEENSE..."
+   pilih = raw_input("USERNAME : ")
+   pilih = raw_input("PASSWORD : ")
+   print "Login Gagal.."
+   time.sleep(3)
+   print "Seperti Nya Anda Tidak Tau Liceense Nya.."
+   os.system('xdg-open https://mytreep.icu/SxRmc')
+   os.sytem('python2 cok.py')
+   
+else:
+    print ('PILIHAN ANDAAAAAA SAALAHHHHH')
+    os.system('python2 cok.py')
